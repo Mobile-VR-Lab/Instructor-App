4c293e9c8f070adff005996cc8b688183c7b7c84
Hannah Marsh <hanmarsh@icloud.com>
2023-12-05 12:10:54 -0500

    remove depracated api call in configure activity becuase it wont let me build project

--- app/src/main/java/com/example/mobilevrlab/screens/activities/ConfigureActivity.java
+++ app/src/main/java/com/example/mobilevrlab/screens/activities/ConfigureActivity.java
@@ -1,10 +1,14 @@
 package com.example.mobilevrlab.screens.activities;
 
+import static com.example.mobilevrlab.screens.controller.ScriptLoader.OPEN_DIRECTORY_CODE;
+
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobilevrlab.R;
@@ -12,14 +16,27 @@ import com.example.mobilevrlab.screens.controller.ScriptLoader;
 
 public class ConfigureActivity extends AppCompatActivity {
 
-    ScriptLoader scriptLoader;
+    private ScriptLoader scriptLoader;
+    // a type-safe method to handle the result of an activity start
+    private ActivityResultLauncher<Intent> fileSelectionActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.configure_activity);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
+
         scriptLoader = new ScriptLoader();
+
+        // Initialize the ActivityResultLauncher
+        fileSelectionActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        scriptLoader.onActivityResult(OPEN_DIRECTORY_CODE, RESULT_OK, result.getData(), 0, getContentResolver());
+                    }
+                }
+        );
     }
 
     // Go back to the home screen if the "x" button is clicked
@@ -28,15 +45,10 @@ public class ConfigureActivity extends AppCompatActivity {
     }
 
     // Open the system file selection dialog when the "select file" button is clicked
-    // note that for testing, a file was put on the emulator at /storage/emulated/0/Download/myscript.xml
     public void onSelectScriptFileClick(View view) {
-        startActivityForResult(scriptLoader.getIntentForFileSelection(), scriptLoader.OPEN_DIRECTORY_CODE);
+        Intent intent = scriptLoader.getIntentForFileSelection();
+        fileSelectionActivityResultLauncher.launch(intent);
     }
 
-    // When the system file selection dialog closes, handle opening the selected XML file
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
-        super.onActivityResult(requestCode, resultCode, resultData);
-        scriptLoader.onActivityResult(requestCode, resultCode, resultData, this.RESULT_OK, getContentResolver());
-    }
+    // the deprecated onActivityResult is no longer needed and has been removed
 }
---
+++ app/src/main/java/com/example/mobilevrlab/screens/activities/HeadsetStatusActivity.java
@@ -0,0 +1,2 @@
+package com.example.mobilevrlab.screens.activities;public class HeadsetStatusActivity {
+}
